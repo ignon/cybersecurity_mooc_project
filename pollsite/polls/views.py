bbc1759d604bba48b4dd1bf1881bf0462abea553
Arttu Mäkinen <arttu.b.makinen@gmail.com>
2026-08-26 16:59:44 +0300

    sql_injection

--- pollsite/polls/views.py
+++ pollsite/polls/views.py
@@ -3,8 +3,22 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
+
+
+class SearchView(generic.ListView):
+    template_name = "polls/search.html"
+    context_object_name = "question_search_results"
+
+    def get_queryset(self):
+      q = self.request.GET.get("q", "")
+      results = Question.objects.raw(
+          "SELECT * FROM polls_question WHERE question_text LIKE '%%%s%%'" % q
+      )
+      return results
+
 
 
 class IndexView(generic.ListView):
@@ -12,13 +26,24 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
+            :5
+        ]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
